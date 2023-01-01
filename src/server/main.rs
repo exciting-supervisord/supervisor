c033576530c94288fd08516df9880bf0ba3930dc
anduuin <jcm2232@gmail.com>
2023-01-01 21:49:51 +0900

    temp

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,24 +1,46 @@
 pub mod core;
 
-use jsonrpc_ipc_server::jsonrpc_core::{IoHandler, Params, Value};
-use jsonrpc_ipc_server::ServerBuilder;
 use lib::config::Config;
+use lib::request::Request;
+use lib::response::Error as RpcError;
 
+use std::io::Read;
 use std::error::Error;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+use std::time::Duration;
+
+use serde_json;
 
 const CONF_FILE: &'static str = "./general.ini";
+
+fn handle_client(mut socket: UnixStream) -> Result<Request, RpcError> {
+    match serde_json::from_reader(socket) {
+        Ok(r) => Ok(r),
+        Err(e) => {
+            
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let conf = Config::from(CONF_FILE)?;
 
-    let mut io = IoHandler::default();
-    io.add_method("add", |_params: Params| async {
-        Ok(Value::String("hello".to_owned()))
-    });
+    let listener = UnixListener::bind("/tmp/secret.sock")?;
+    listener.set_nonblocking(true)?;
 
-    let server = ServerBuilder::new(io).start(&conf.general.sockfile)?;
-
-    server.wait();
-
-    Ok(())
+    loop {
+        match listener.accept() {
+            Ok((socket, addr)) => {
+                match handle_client(socket) {
+                    Ok(v) => println!("received: {:?}", v),
+                    Err(e) => println!("err: {}", e)
+                };
+            }
+            Err(e) => {
+                println!("error occered: {}", e);
+                thread::sleep(Duration::from_secs(1));
+            }
+        }
+    }
 }
