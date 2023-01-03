6a0a63dc6b7077491091540b5c5f163daa26b427
cjeon <cjeon@student.42seoul.kr>
2023-01-03 00:16:32 +0000

    Fix error message

--- src/lib/response.rs
+++ src/lib/response.rs
@@ -5,7 +5,7 @@ pub type Response = Result<OutputMessage, Error>;
 #[derive(Debug, Deserialize, Serialize)]
 pub enum Error {
     FileFormat,
-    Service,
+    Service(String),
     ProcessNotFound(String),
     ProcessNotRunning(String),
     ProcessAlreadyStarted(String),
@@ -15,10 +15,10 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::FileFormat => write!(f, "Invalid configuraion file."),
-            Error::Service => write!(f, "Service temporary unavailable."),
-            Error::ProcessNotFound(ref s) => write!(f, "{s}: no such process "),
-            Error::ProcessNotRunning(ref s) => write!(f, "{s}: not running"),
-            Error::ProcessAlreadyStarted(ref s) => write!(f, "{s}: already started"),
+            Error::Service(ref s) => write!(f, "{s}: Service not available."),
+            Error::ProcessNotFound(ref s) => write!(f, "{s}: no such process."),
+            Error::ProcessNotRunning(ref s) => write!(f, "{s}: not running."),
+            Error::ProcessAlreadyStarted(ref s) => write!(f, "{s}: already started."),
         }
     }
 }
