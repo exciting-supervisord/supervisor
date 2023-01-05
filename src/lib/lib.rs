6190d5fe0d76a284d00c5bb2ca27546ce9ea8e42
mypark <mypark@debian-c1r2s3.mypark.42.kr>
2023-01-04 22:37:16 -0600

    feat: config obj in client

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -5,6 +5,8 @@ pub mod process_status;
 pub mod request;
 pub mod response;
 
+pub const CONF_FILE: &'static str = "./general.ini";
+
 pub fn exit_with_error(err: Box<dyn std::error::Error>) -> ! {
     eprintln!("{err}");
     std::process::exit(1)
